2f9571d0d73ef83f66449f96e9343bfba848bbca
TakuProsper <takueprosper00@gmail.com>
2024-01-14 21:26:14 +0200

    code update

--- django1/studybud/base/views.py
+++ django1/studybud/base/views.py
@@ -64,7 +64,8 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains=q)|Q(name__icontains=q)|Q(description__icontains=q))
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__name__icontains=q))
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count,'room_messages':room_messages}
     return render(request, 'base/home.html', context) 
 
 def room(request,pk):
@@ -77,6 +78,8 @@ def room(request,pk):
             room=room,
             body=request.POST.get('body')
         )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
     context = {'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request,'base/room.html', context)
 
@@ -119,3 +122,15 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    
+     
+    if request.user != message.user:
+        return HttpResponse('you are not allowed here!!')
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':message})
