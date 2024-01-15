fd7d1645d2dcaca854fe4db1de15a69164050e9e
TakuProsper <takueprosper00@gmail.com>
2024-01-15 23:55:49 +0200

    updating user profile backend

--- django1/studybud/base/views.py
+++ django1/studybud/base/views.py
@@ -86,7 +86,9 @@ def room(request,pk):
 def userProfile(request,pk):
     user = User.objects.get(username=pk)
     rooms = user.room_set.all()
-    context={"user":user, "rooms":rooms}
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context={"user":user, "rooms":rooms,'room_messages':room_messages,'topics':topics  }
     return render(request, "base/profile.html", context)
 
 @login_required(login_url='login')
