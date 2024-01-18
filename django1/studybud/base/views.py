daf9450cd622002ca322cc71328c0f6fef7364bb
TakuProsper <takueprosper00@gmail.com>
2024-01-18 22:23:18 +0200

    Updating the user profile update theme and the back-end

--- django1/studybud/base/views.py
+++ django1/studybud/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 #rooms = [
 #    {'id':1, 'name':'Lets Learn Python!'},
@@ -152,3 +152,16 @@ def deleteMessage(request,pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect ('user-profile', pk=user.username)
+    context={'form':form}
+    return render(request, 'base/update-user.html', context)
