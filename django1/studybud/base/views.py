29578d59685975b910417c19cab526ede65f0e7f
TakuProsper <takueprosper00@gmail.com>
2024-01-19 22:21:09 +0200

    Creating a mobile responsive Browse topics page and programming the back-end

--- django1/studybud/base/views.py
+++ django1/studybud/base/views.py
@@ -62,7 +62,7 @@ def registerUser(request):
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(Q(topic__name__icontains=q)|Q(name__icontains=q)|Q(description__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {'rooms':rooms, 'topics':topics, 'room_count':room_count,'room_messages':room_messages}
@@ -165,3 +165,8 @@ def updateUser(request):
             return redirect ('user-profile', pk=user.username)
     context={'form':form}
     return render(request, 'base/update-user.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html',{'topics':topics})
