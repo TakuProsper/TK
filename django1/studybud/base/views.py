0ff611aca9bafed5c3c81c519ccf0e1d0fa82ee2
TakuProsper <takueprosper00@gmail.com>
2024-01-19 22:36:05 +0200

    creating a mobile responsive activities page and linking up with the backend

--- django1/studybud/base/views.py
+++ django1/studybud/base/views.py
@@ -170,3 +170,8 @@ def topicsPage(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     topics = Topic.objects.filter(name__icontains=q)
     return render(request, 'base/topics.html',{'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages':room_messages}
+    return render(request, 'base/activity.html', context)
