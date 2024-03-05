d63e92e393755016bf6b2b1d58dd683b7833c133
TakuProsper <94491624+TakuProsper@users.noreply.github.com>
2024-03-05 14:50:59 +0200

    Updating the register user form

--- django1/studybud/base/views.py
+++ django1/studybud/base/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib.auth import authenticate, login,logout
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm,MyUserCreationForm
 
 #rooms = [
 #    {'id':1, 'name':'Lets Learn Python!'},
@@ -44,14 +43,14 @@ def LogoutUser(request):
     return redirect('login')
 
 def registerUser(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            login(request,user)
+            login(request.user)
             return redirect('home')
         else:
             messages.error(request,"An error occured during registration, please try again. ")
